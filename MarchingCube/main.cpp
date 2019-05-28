3028d55d5ea129b66324585f97c395c4ee732fc0
redleafliang <xialuokecaocao@outlook.com>
2019-05-28 11:08:37 +0800

    Initial Commit

---
+++ MarchingCube/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MarchingCube
+//
+//  Created by Hongye Liang on 2019/5/28.
+//  Copyright © 2019 Hongye Liang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
